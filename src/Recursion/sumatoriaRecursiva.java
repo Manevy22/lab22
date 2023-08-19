33ff778c37272d19e03d4b2d42c709ae95deb75a
Manevy22 <141934385+Manevy22@users.noreply.github.com>
2023-08-19 18:50:39 -0300

    Update sumatoriaRecursiva.java

--- src/Recursion/sumatoriaRecursiva.java
+++ src/Recursion/sumatoriaRecursiva.java
@@ -5,14 +5,15 @@ package Recursion;
 import java.util.Scanner;
 
 public class sumatoriaRecursiva {
-    public static void main(String[] args){
+    public static void main(String[] args){//Clase principal.
         Scanner scanner=new Scanner(System.in);
-        System.out.println("Ingrese un valor para calcular su sumatoria");
+        System.out.println("Ingrese un valor para calcular su sumatoria");//Se pide al usuario que ingrese un valor.
         int val=scanner.nextInt();
-        System.out.println("La sumatoria del valor ingresado es: "+sumatoria(val));
+        System.out.println("La sumatoria del valor ingresado es: "+sumatoria(val));//Se llama al método que aplica recursión y se muestra el resultado por pantalla.
     }
     public static int sumatoria(int val){
-        if(val==1)return 1;
-        return val+sumatoria(val-1);
+        if(val==1)return 1;//Condición de salida.
+        return val+sumatoria(val-1);//Se llama a la función nuevamente.
+        //Se suma el valor actual con su antecesor, y así sucesivamente, hasta llegar a 1.
     }
 }
