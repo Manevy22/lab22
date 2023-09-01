a20aba711125f0d8e7db6903179bd256cd2333cb
Manevy22 <141934385+Manevy22@users.noreply.github.com>
2023-09-01 00:53:29 -0300

    Update Impuesto.java

--- src/SistemaGestionEmpleados/Impuesto.java
+++ src/SistemaGestionEmpleados/Impuesto.java
@@ -1,5 +1,5 @@
 package SistemaGestionEmpleados;
-
+//Interfaz que permite a cada sub-clase de Empleado calcular su sueldo. 
 public interface Impuesto {
     public abstract double calcularImpuesto();
 
