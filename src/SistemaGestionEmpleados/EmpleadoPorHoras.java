cac684669bb037cf75cef4dbb0377fa44bbd8f62
Manevy22 <141934385+Manevy22@users.noreply.github.com>
2023-09-01 00:48:14 -0300

    Update EmpleadoPorHoras.java

--- src/SistemaGestionEmpleados/EmpleadoPorHoras.java
+++ src/SistemaGestionEmpleados/EmpleadoPorHoras.java
@@ -1,17 +1,17 @@
 package SistemaGestionEmpleados;
 
 public class EmpleadoPorHoras extends Empleado implements Impuesto{
-    protected int horasTrabajadas;
+    protected int horasTrabajadas;//Atributo de la subclase EmpleadoPorHora
     public EmpleadoPorHoras(String nombre,int horasTrabajadas,double sueldoBase){
         super(sueldoBase,nombre);
         this.horasTrabajadas=horasTrabajadas;
     }
-
+//En este caso el parámetro de sueldoBase me devuelve las ganancias del empleado por hora. 
     @Override
     public double calcularSueldo() {
         return horasTrabajadas*sueldoBase;
     }
-
+    //Los impuestos a pagar de cada Empleado es un 35% de su suelo. 
     @Override
     public double calcularImpuesto() {
         return this.calcularSueldo()*0.35;
@@ -20,7 +20,7 @@ public class EmpleadoPorHoras extends Empleado implements Impuesto{
     public void setHorasTrabajadas(int horasTrabajadas) {
         this.horasTrabajadas = horasTrabajadas;
     }
-
+//Se muestra la información del empleado. 
     @Override
     public String toString() {
         return "Nombre: "+nombre+",sueldo: "+calcularSueldo()+",impuestos:"+calcularImpuesto();
