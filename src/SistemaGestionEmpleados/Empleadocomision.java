cc04eb6dd718b3dab00fc08bc4bc1e26761ff57a
Manevy22 <141934385+Manevy22@users.noreply.github.com>
2023-09-01 00:50:45 -0300

    Update Empleadocomision.java

--- src/SistemaGestionEmpleados/Empleadocomision.java
+++ src/SistemaGestionEmpleados/Empleadocomision.java
@@ -1,7 +1,7 @@
 package SistemaGestionEmpleados;
 
 public class Empleadocomision extends Empleado implements Impuesto {
-    protected double VentasRealizadas;
+    protected double VentasRealizadas;//Atributo especial del EmpleadoComision.
     public Empleadocomision(double sueldoBase, String nombre, double VentasRealizadas, double Comision){
         super(sueldoBase,nombre);
        this.VentasRealizadas=VentasRealizadas*Comision;
@@ -10,13 +10,14 @@ public class Empleadocomision extends Empleado implements Impuesto {
     @Override
     public double calcularSueldo() {
         return sueldoBase+VentasRealizadas;
+        //Para calcular su sueldo se le suma al sueldo base la comision por venta. 
     }
-    public double calcularImpuesto() {
+    public double calcularImpuesto() {    //Los impuestos a pagar de cada Empleado es un 35% de su suelo. 
         return this.calcularSueldo()*0.35;
     }
 
     @Override
-    public String toString() {
+    public String toString() {//Devuelve la información del empleado. 
         return "Nombre: "+nombre+",sueldo: "+calcularSueldo()+",impuestos:"+calcularImpuesto();
     }
 }
